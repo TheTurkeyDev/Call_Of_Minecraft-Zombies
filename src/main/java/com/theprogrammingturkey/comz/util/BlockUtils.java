0ea6ce5cb1cd1ca7c4fcfa75ccfdbbcd9019b186
Turkey2349 <turkey2349@gmail.com>
2020-04-26 01:21:08 -0400

    Fix for wall signs Temporarily added spigot jar for remote building

--- src/main/java/com/theprogrammingturkey/comz/util/BlockUtils.java
+++ src/main/java/com/theprogrammingturkey/comz/util/BlockUtils.java
@@ -2,11 +2,22 @@ package com.theprogrammingturkey.comz.util;
 
 import org.bukkit.Material;
 import org.bukkit.block.data.type.Sign;
+import org.bukkit.block.data.type.WallSign;
 
 public class BlockUtils
 {
-	public static boolean isSign(Material mat)
+	public static boolean isWallSign(Material mat)
+	{
+		return mat.data == WallSign.class;
+	}
+
+	public static boolean isStandingSign(Material mat)
 	{
 		return mat.data == Sign.class;
+	}
+
+	public static boolean isSign(Material mat)
+	{
+		return isStandingSign(mat) || isWallSign(mat);
 	}
 }
