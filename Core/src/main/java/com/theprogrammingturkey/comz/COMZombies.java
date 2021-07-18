6155d2880d987979c3aa4d25691306d0b1e77832
Triz <kiebusiness1@gmail.com>
2021-07-18 17:04:57 +0100

    Marked support for 1.17.1

--- Core/src/main/java/com/theprogrammingturkey/comz/COMZombies.java
+++ Core/src/main/java/com/theprogrammingturkey/comz/COMZombies.java
@@ -130,6 +130,9 @@ public class COMZombies extends JavaPlugin
 			case "1.17":
 				nmsUtil = new NMSUtil_1_17_R1();
 				break;
+			case "1.17.1":
+				nmsUtil = new NMSUtil_1_17_R1();
+				break;
 			default:
 				throw new IllegalStateException("Sorry, COM:Z Does not current support server version" + version);
 		}
